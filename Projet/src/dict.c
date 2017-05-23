fca034f003a5c6f50b5c14f1d0d462981f55302a
EnzoMolion <enzo.molion@e.ujf-grenoble.fr>
2017-05-23 11:11:16 +0200

    Correction beug mais seg fault

--- Projet/src/dict.c
+++ Projet/src/dict.c
@@ -4,6 +4,56 @@
 #include "dict.h"
 
 
+dict_error_t dict_rechercher_mot(dict_t dico, char* mot, int taille_mot, dict_index_t* resultat){
+        noeud_t noeud_courant = dico->racine;
+        int i = 0;
+        dict_error_t error = DICT_NOERROR;
+        while (i < taille_mot && error != DICT_NOTFOUND) {
+                while (noeud_courant->sym != mot[i] && noeud_courant != NULL) {
+                        noeud_courant = noeud_courant->frere;
+                }
+                if (noeud_courant->sym == mot[i]) {
+                        if (i == taille_mot) {
+                                *resultat = noeud_courant->code;
+                        }else if (noeud_courant->fils == NULL) {
+                                error = DICT_NOTFOUND;
+                        }else {
+                                noeud_courant = noeud_courant->fils;
+                        }
+                }else{
+                        error = DICT_NOTFOUND;
+                }
+        }
+        return error;
+}
+
+
+dict_error_t dict_rechercher_index(dict_t dico, dict_index_t index, char* resultat){
+        int i = 0;
+        int j = 1;
+        for ( i = 0; i < dico->nb_elt || dico->map[i]->code == index; i++) {
+        }
+        if ( dico->map[i]->code == index) {
+                noeud_t noeud_courant = dico->map[i];
+                resultat[0] = noeud_courant->sym;
+                while (noeud_courant->pere != NULL) {
+                        noeud_courant = noeud_courant->pere;
+                        resultat[j] = noeud_courant->sym;
+                        j++;
+                }
+        }else{
+                return DICT_NOTFOUND;
+        }
+        char temp;
+        for (int k = 0; k <= j/2; k++) {
+                temp = resultat[k];
+                resultat[k] = resultat[j-k];
+                resultat[j-k] = temp;
+        }
+        return DICT_NOERROR;
+}
+
+
 // Retourne le pointeur du noeud ayant c pour symbole et son frere precedent
 // Les deux pointeurs vallent NULL si c n'est pas present
 noeud_t rechercher_dans_ligne(noeud_t liste, char c, noeud_t frere){
@@ -37,6 +87,7 @@ dict_error_t ajouter_dans_ligne(dict_t dico, noeud_t pere, noeud_t frere, char s
 	courant->fils = NULL ;
 	courant->frere = NULL ;
 	courant->pere = pere ;
+	dico->map[dico->nb_elt] = courant;
 
 	return DICT_NOERROR ;
 }
@@ -50,7 +101,7 @@ dict_error_t dict_insert(dict_t dico, char* mot, int taille_mot){
 	int index_caractere_courant = 0;
 	noeud_t noeud_courant ;
 	noeud_t noeud_pere ;
-	noeud_t noeud_frere ;
+	noeud_t noeud_frere = NULL;
 
 	if (dict_rechercher_mot(dico, mot, taille_mot, &index) == DICT_NOTFOUND){
 		while (index_caractere_courant < taille_mot){
@@ -74,7 +125,7 @@ dict_error_t dict_insert(dict_t dico, char* mot, int taille_mot){
 
 void dict_print (dict_t dico){
 
-	char* mot_courant ;
+	char* mot_courant = NULL;
 
 	for (int i = 0; i < dico->nb_elt; i++){
 		dict_rechercher_index(dico, i, mot_courant);
@@ -90,7 +141,7 @@ dict_t dict_new(){
 
 	dico->nb_elt = 0 ;
 
-	char * mot_courant ;
+	char * mot_courant = NULL;
 
 	mot_courant[1] = '\0' ;
 
@@ -106,50 +157,6 @@ dict_t dict_new(){
 	return dico ;
 }
 
-dict_error_t dict_rechercher_mot(dict_t dico, char* mot, int taille_mot, dict_index_t resultat){
-        noeud_t noeud_courant = dico->racine;
-        int i = 0;
-        dict_error_t error = DICT_NOERROR;
-        while (i < taille_mot && error != DICT_NOTFOUND) {
-                while (noeud_courant->sym != mot[i] && noeud_courant != NULL) {
-                        noeud_courant = noeud_courant->frere;
-                }
-                if (noeud_courant->sym == mot[i]) {
-                        if (i == taille_mot) {
-                                resultat = noeud_courant->code;
-                        }else if (noeud_courant->fils == NULL) {
-                                error = DICT_NOTFOUND;
-                        }else {
-                                noeud_courant = noeud_courant->fils;
-                        }
-                }else{
-                        error = DICT_NOTFOUND;
-                }
-        }
-        return error;
-}
 
-dict_error_t dict_rechercher_index(dict_t dico, dict_index_t index, char* resultat){
-        int i = 0;
-        int j = 1;
-        for ( i = 0; i < dico->nb_elt || dico->hashmap[i]->code == index; i++) {
-        }
-        if ( dico->hashmap[i]->code == index) {
-                noeud_t noeud_courant = dico->hashmap[i];
-                resultat[0] = noeud_courant->sym;
-                while (noeud_courant->pere != NULL) {
-                        noeud_courant = noeud_courant->pere;
-                        resultat[j] = noeud_courant->sym;
-                        j++;
-                }
-        }else{
-                return DICT_NOTFOUND;
-        }
-        char temp;
-        for (int k = 0; k <= j/2; k++) {
-                temp = resultat[k];
-                resultat[k] = resultat[j-k];
-                resultat[j-k] = temp;
-        }
-        return DICT_NOERROR;
-}
+
+
