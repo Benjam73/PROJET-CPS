de72cbde49c946e96215e41cef87523ac5f1a392
leovalette <leovalette38@gmail.com>
2017-05-23 15:43:17 +0200

    compression

--- Projet/src/codage.c
+++ Projet/src/codage.c
@@ -6,35 +6,42 @@
 
 
 uint8_t* concatenation (uint8_t* w, int wlength, uint8_t* a){ //longueur en nombre d'octets
-  uint8_t* wa;
-  int i = 0;
-  for(i = 0 ; i < wlength ; i++){
-    wa[i] = w[i];
-  }
-  wa[i+1] = a[0];
-  return wa;
+        uint8_t* wa = NULL;
+        int i = 0;
+        for(; i < wlength; i++) {
+                wa[i] = w[i];
+        }
+        wa[i+1] = a[0];
+        return wa;
 }
 
 
 
-FILE codage (FILE *f){
-  uint8_t* a;
-  uint8_t* w;
+FILE* codage (FILE *f){
+        uint8_t* a = NULL;
+        uint8_t* w = NULL;
 
-  dict_t dico = dict_new();
-  dict_index_t *index;
+        FILE *fout = NULL;
 
-  fread(w, 1, 1, f);
-  int wlength = 1;
-  while (!feof(f)) {
-       fread(a, 1, 1, f);
-       if (dict_rechercher_mot(dico, concatenation(w, wlength, a) , wlength+1 , index) ==  DICT_NOERROR){
-         w = concatenation(w, wlength, a);
-         wlength++;
-       }else{
-         //TODO écrire sur le fichier de sortie l'index associé à w dans D
-         dict_insert(dico, wlength, w);
-         w = a;
-       }
-  }
+        dict_t dico = dict_new();
+        dict_index_t *index = NULL;
+
+        fread(w, 1, 1, f);
+        int wlength = 1;
+        while (!feof(f)) {
+                fread(a, 1, 1, f);
+                if (dict_rechercher_mot(dico, concatenation(w, wlength, a), wlength+1, index) ==  DICT_NOERROR) {
+                        w = concatenation(w, wlength, a);
+                        wlength++;
+                }else{
+                        //TODO écrire sur fout l'index associé à w dans D
+                        dict_rechercher_mot(dico, w, wlength+1, index);
+
+                        dict_insert(dico,w, wlength);
+                        w = a;
+                }
+        }
+        //TODO écrire sur fout l'index associé à w dans D
+        dict_rechercher_mot(dico, w, wlength+1, index);
+        return fout;
 }
