6c1d369f097e6683012a7c6361670afb997c123b
EnzoMolion <enzo.molion@e.ujf-grenoble.fr>
2017-05-24 15:50:09 +0200

    Ajout main

--- Projet/src/test_dict.c
+++ Projet/src/test_dict.c
@@ -1,23 +1,60 @@
 #include <stdlib.h>
 #include "dict.h"
 #include "LZW.h"
+#include <string.h>
+
+#define true 1
+#define false 0
+
 
 int main(int argc, char const *argv[]){
+
+	int argument_aide = false, comp = false, decomp =false ;
 	
-	FILE* f_input ;
-	FILE* f_output ;
+
+	FILE *f_input_c, *f_output_c, *f_input_x, *f_output_x ;
 	dict_t dico ;
+
+	
+	for (int i = 1 ; i < argc && !argument_aide ; i++){
+		if (strcmp(argv[i], "-h") == 0){
+			argument_aide = true ;
+		}
+	}
+
+	if(argc < 3 || argc > 5 || argument_aide){
+		fprintf(stdout, "%s [-c fichier_entree ficher_sortie] [-x fichier_entree ficher_sortie]\n", argv[0]);
+		fprintf(stdout, "Arguments : \n\t -c : compression\n\t -x : decompression\n");
+		return -1 ;
+	}
+
+
+	for (int i = 1 ; i < argc && !(comp && decomp) ; i++){
+		if (strcmp(argv[i], "-c") == 0){
+			f_input_c = fopen(argv[i + 1], "r");
+			f_output_c = fopen(argv[i + 2], "w");
+			comp = true ;
+		}
+		if (strcmp(argv[i], "-x") == 0){
+			f_input_x = fopen(argv[i + 1], "r");
+			f_output_x = fopen(argv[i + 2], "w");
+			decomp = true ;
+		}
+	}
+
 
 
 	dico = dict_new();
 	dict_print(dico);
 
-	f_input = fopen(argv[1], "r");
-	f_output = fopen(argv[2], "w");
 
-	compression (f_input, f_output);
+	if (comp){
+		compression (f_input_c, f_output_c);
+	}
 
-	decompression (f_input, f_output);
+	if (decomp){
+		decompression (f_input_x, f_output_x);
+	}
 
 
 	return 0;
