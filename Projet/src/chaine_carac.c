c5a45dabc850174695923e7eb08dcd68eeb72006
EnzoMolion <enzo.molion@e.ujf-grenoble.fr>
2017-05-24 17:01:36 +0200

    ajout dossier entree-sortie

--- Projet/src/chaine_carac.c
+++ Projet/src/chaine_carac.c
@@ -3,13 +3,17 @@
 void fprintf_n_octets(FILE* f, uint8_t* word, int n){
 	for (int i = 0; i < n; i++){
 		fprintf(f, "%c", word[i]);
+		fprintf(stdout, "%s\n", "On ecrit");
 	}
+	fflush(f);
 }
 
 void fprintf_n_octets_comp(FILE* f, dict_index_t* index, int n){
 	for (int i = 0; i < n; i++){
 		fprintf(f, "%c", index[i]);
+		fprintf(stdout, "%s\n", "On ecrit");		
 	}
+	fflush(f);
 }
 
 uint8_t* concatenation(uint8_t* w, int wlength, uint8_t* a){
