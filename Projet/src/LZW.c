85d4f1d1b470e51f0cca1166eac918fa74355d85
Benjam73 <besnierbenjamin73@gmail.com>
2017-05-26 17:04:31 +0200

    aucun changement

--- Projet/src/LZW.c
+++ Projet/src/LZW.c
@@ -56,12 +56,82 @@ void compression (FILE* f_input, FILE* f_output){
 	fprintf_n_octets_comp(f_output, &index, taille);
 }
 
+// void decompression (FILE* f_input, FILE* f_output){
+
+// 	dict_t dico ;
+
+// 	dict_index_t i1 = 0; 
+// 	dict_index_t i2 = 0;
+
+// 	uint8_t* w1 = malloc(sizeof(uint8_t)) ;
+// 	uint8_t* w2 = malloc(sizeof(uint8_t)) ;
+// 	int len_w1 = 0, len_w2 = 0;
+
+// 	uint8_t a[1] ;
+
+
+//         // D <- ens. de toutes les chaînes de longueur 1
+// 	dico = dict_new();
+
+//         // i' <- 1er code de S
+// 	fread(&i1, 1, 1, f_input) ;
+//         #ifdef DEBUG
+// 	printf("On a lu %d\n", i1);
+//         #endif
+
+//         // a <- chaine d'index i dans D
+// 	dict_rechercher_index(dico, i1, a);
+
+//         // w <- [a]
+// 	w1[0] = a[0] ;
+// 	len_w1 = 1 ;
+
+//         // ecrire w sur E
+// 	fprintf_n_octets(f_output, w1, len_w1);
+
+//         // tant que la fin de S n'est pas atteinte
+// 	while(!feof(f_input)){
+
+//                 // i' <- code suivant de S
+// 		fread(&i2, 1, 1, f_input);
+//                 // #ifdef DEBUG
+//                 // printf("On a lu %d\n", i2);
+//                 // #endif
+
+//                 // On place la chaine d'index i2 dans w2
+//                 // Si on a une erreur, c'est que i2 n'est pas dans dico
+// 		if (dict_rechercher_index(dico, i2, w2) != DICT_NOERROR){
+// 			dict_rechercher_index(dico, i1, w2) ;
+// 			w2[len_w2] = a[0] ; 
+// 		}
+
+//                 // ecrire w' sur E 
+// 		fprintf_n_octets(f_output, w2, len_w2);
+
+//                 // a <- 1er octet de w' 
+// 		a[0] = w2[0] ;
+
+//                 // D <- D U {w.a}
+// 		dict_insert(dico, concatenation(w1, len_w1, a), len_w1+1);
+
+//                 // i <- i'
+// 		i1 = i2 ;
+
+//                 // w <- chaine d'index i dans D
+// 		dict_rechercher_index(dico, i1, w1);
+
+// 	}
+
+// }
+
 void decompression (FILE* f_input, FILE* f_output){
 
 	dict_t dico ;
 
 	dict_index_t i1 = 0; 
 	dict_index_t i2 = 0;
+	dict_index_t i3 = 0;
+
 
 	uint8_t* w1 = malloc(sizeof(uint8_t)) ;
 	uint8_t* w2 = malloc(sizeof(uint8_t)) ;
@@ -73,10 +143,21 @@ void decompression (FILE* f_input, FILE* f_output){
         // D <- ens. de toutes les chaînes de longueur 1
 	dico = dict_new();
 
+	// Lecture du premier index
         // i' <- 1er code de S
 	fread(&i1, 1, 1, f_input) ;
         #ifdef DEBUG
 	printf("On a lu %d\n", i1);
+        #endif
+
+	fread(&i2, 1, 1, f_input) ;
+        #ifdef DEBUG
+	printf("On a lu %d\n", i2);
+        #endif
+	
+	fread(&i3, 1, 1, f_input) ;
+        #ifdef DEBUG
+	printf("On a lu %d\n", i3);
         #endif
 
         // a <- chaine d'index i dans D
@@ -85,6 +166,18 @@ void decompression (FILE* f_input, FILE* f_output){
         // w <- [a]
 	w1[0] = a[0] ;
 	len_w1 = 1 ;
+
+	dict_rechercher_index(dico, i2, a);
+
+        // w <- [a]
+	w1[1] = a[0] ;
+	len_w1++ ;
+
+	dict_rechercher_index(dico, i3, a);
+
+        // w <- [a]
+	w1[2] = a[0] ;
+	len_w1++ ;
 
         // ecrire w sur E
 	fprintf_n_octets(f_output, w1, len_w1);
