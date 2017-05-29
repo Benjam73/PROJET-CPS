aa8a32bb40372c2eb0f13cba35af7fc80a4d1ac9
leovalette <leovalette38@gmail.com>
2017-05-30 01:03:26 +0200

    nouveau RLE

--- Projet/RLE/src/RLE.c
+++ Projet/RLE/src/RLE.c
@@ -11,22 +11,23 @@ void rle (FILE* f_input, FILE* f_output){
 	while (!feof(f_input)){
 
 		fscanf(f_input, "%c", &courant) ;
-		if (compare == courant){
+		if (compare == courant && cpt <= 9){
 			cpt++;
 		}
 		else{
             // Si le compteur est superieur a 1 alors il y a une repetition du char compare.
-			if (cpt>1){
-				fprintf(f_output,"%d", cpt);
-				fprintf(f_output,"%c", compare);
-			}
+			if (cpt>=2){
+                fprintf(f_output,"%c", compare);
+                fprintf(f_output,"%c", compare);
+                fprintf(f_output,"%d", cpt-2);
+            }
             else{ 
             // Sinon il n'y a pas de repetion donc on affiche que le char.
                 fprintf(f_output,"%c", compare);
             }
 
             // Permettant de mettre un point entre chaque couple (int, char), sert alors de separateur
-            fprintf(f_output, ".");
+            // fprintf(f_output, ".");
             compare = courant;
             cpt = 1;
         }
@@ -34,45 +35,32 @@ void rle (FILE* f_input, FILE* f_output){
 }
 
 void elr (FILE* f_input, FILE* f_output){
-    int entier = 0, unite = 0, new_cpt = 0;
-    char carac;
+    int entier = 0;//, unite = 0, new_cpt = 0;
+    char courant ;
+    char compare ;
+    
+    fscanf(f_input, "%c", &courant) ;
+
 
     while (!feof(f_input)) {
 
         // Consomme et met dans une variable l'entier, le caractere et consomme juste le point.
-        int scan_int = fscanf(f_input,"%d", &entier);
-        fscanf(f_input,"%c", &carac);
-        fscanf(f_input,".");
-
+        fscanf(f_input,"%c", &compare);
+        
         // Si le caractere est un point c'est qu'il a une entree : .123. Ici on doit alors afficher 12 fois le chiffre 3.
-        if (carac == '.'){
-            // Si l'entier est inferieur a 10 alors on a juste a mettre le chiffre dans le fichier.
-            if (entier<10){
-                fprintf(f_output, "%d", entier);
-            }else{ 
-                // Sinon c'est qu'il y a une repetion d'un chiffre
-                // Recupere l'unite de l'entier qui est l'entier a repeter
-                unite = entier%10;
-                // Recupere le nombre de fois qu'il faut repeter l'entier unite
-                new_cpt = (entier-unite)/10;
-                while(new_cpt != 0){
-                    // Met unite le nombre de fois necessaire
-                    fprintf(f_output, "%d", unite);
-                    new_cpt--;
-                }
+        if (courant == compare){
+            fscanf(f_input, "%d", &entier);
+            fprintf(f_output,"%c",compare);
+            fprintf(f_output,"%c",compare);
+            while(entier > 0){
+                fprintf(f_output,"%c",compare);
+                entier--;
             }
-        }else{
-            // Si le scanf recuperant l'entier n'a pas reussi a trouver d'entier alors on met dans le fichier le caractere une seule fois car il n'y a pas de repetition de celui-ci
-            if (scan_int == 0){
-                fprintf(f_output, "%c", carac);
-            }else{ 
-                // Sinon on met dans le fichier f_output le caractere le nombre de fois donne par l'entier
-                while(entier != 0){
-                    fprintf(f_output, "%c", carac);
-                    entier--;
-                }
-            }
+        }else{ 
+            fprintf(f_output,"%c",courant);
+            fscanf(f_input, "%c", &courant) ;
         }
-    }   
+
+    }
 }
 
