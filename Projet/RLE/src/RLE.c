58f62df152af7ca29ac588322d83606fce8bfa08
bastien <dearaujo.bastien@gmail.com>
2017-05-29 16:02:28 +0200

    code fichier rle commenté

--- Projet/RLE/src/RLE.c
+++ Projet/RLE/src/RLE.c
@@ -73,14 +73,15 @@ void rle (FILE* f_input, FILE* f_output){
 			cpt++;
 		}
 		else{
+            // Si le compteur est supérieur à 1 alors il y a une répétition du char compare.
 			if (cpt>1){
 				fprintf(f_output,"%d", cpt);
 				fprintf(f_output,"%c", compare);
-			}
-			else{
+			}else{ // Sinon il n'y a pas de répétion donc on affiche que le char.
 				fprintf(f_output,"%c", compare);
-
 			}
+
+            // Permettant de mettre un point entre chaque couple (int, char), sert alors de séparateur
 			fprintf(f_output, ".");
 			compare = courant;
 			cpt = 1;
@@ -93,24 +94,33 @@ void elr (FILE* f_input, FILE* f_output){
     char carac;
 
     while (!feof(f_input)) {
+
+        // Consomme et met dans une variable l'entier, le caractère et consomme juste le point.
         int scan_int = fscanf(f_input,"%d", &entier);
        	fscanf(f_input,"%c", &carac);
         fscanf(f_input,".");
+
+        // Si le caractère est un point c'est que il a une entrée : .123. Ici on doit alors afficher 12 fois le chiffre 3.
         if (carac == '.'){
+            // Si l'entier est inférieur à 10 alors on a juste à mettre le chiffre dans le fichier.
             if (entier<10){
                 fprintf(f_output, "%d", entier);
-            }else{
-                int new_entier = entier%10;
-                int new_cpt = (entier-new_entier)/10;
+            }else{ // Sinon C'est qu'il y a une répétion d'un chiffre
+                // Récupère l'unité de l'entier qui est l'entier a répéter
+                int unite = entier%10;
+                // Récupère le nombre de fois qu'il faut répéter l'entier unite
+                int new_cpt = (entier-unite)/10;
                 while(new_cpt != 0){
-                    fprintf(f_output, "%d", new_entier);
+                    // Met unite le nombre de fois nécessaire
+                    fprintf(f_output, "%d", unite);
                     new_cpt--;
                 }
             }
         }else{
+            // Si le scanf récupérant l'entier n'a pas réussi à trouver d'entier alors on met dans le fichier le caractère une seule fois car il n'y a pas de répétition de celui-ci
             if (scan_int == 0){
                 fprintf(f_output, "%c", carac);
-            }else{
+            }else{ // Sinon on met dans le fichier f_output le caractère le nombre de fois donné par l'entier
                 while(entier != 0){
                     fprintf(f_output, "%c", carac);
                     entier--;
@@ -169,7 +179,7 @@ int main(int argc, char *argv[]){
 		if (f_output_c == NULL){
 			fprintf(stdout, "%s\n", "Erreur ouverture ficher sortie compression");
 			return 1 ;
-		}		
+		}
 	}
 	if(decomp){
 		if (f_input_x == NULL){
