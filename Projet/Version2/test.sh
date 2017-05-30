7bc86c65ca1c6143be7164ddb5a9a9d84ea83acb
Benjam73 <besnierbenjamin73@gmail.com>
2017-05-30 14:21:59 +0200

    ajout test

--- Projet/Version2/test.sh
+++ Projet/Version2/test.sh
@@ -11,9 +11,34 @@ echo "Taille avant compression"
 echo $a
 echo "Taille apres compression"
 echo $b
+z="($x/$a)"
+echo "Taux de compression"
+echo "$z*$b" |bc -l 
 bin/BAMV_LZW -x output/dicoAB_comp.txt output/dicoAB_decomp.txt
 echo""
 echo""
 echo""
 echo "Difference fichier avant/apres compression/decompression"
 echo $(diff input/dicoAB.txt output/dicoAB_decomp.txt)
+
+echo "----------------------------------------------"
+
+bin/BAMV_LZW -c input/lorem_ipsum.txt output/lorem_ipsum_comp.txt
+a=$(wc -c<input/lorem_ipsum.txt)
+b=$(wc -c<output/lorem_ipsum_comp.txt)
+echo""
+echo "Pour le fichier lorem_ipsum.txt :"
+echo""
+echo "Taille avant compression"
+echo $a
+echo "Taille apres compression"
+echo $b
+z="($x/$a)"
+echo "Taux de compression"
+echo "$z*$b" |bc -l
+bin/BAMV_LZW -x output/lorem_ipsum_comp.txt output/lorem_ipsum_decomp.txt
+echo""
+echo""
+echo""
+echo "Difference fichier avant/apres compression/decompression"
+echo $(diff input/lorem_ipsum.txt output/lorem_ipsum_decomp.txt)
