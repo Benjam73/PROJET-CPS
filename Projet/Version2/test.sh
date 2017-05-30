3c1bc05f1dd95fe6c9e8cce945b12e9427085346
Benjam73 <besnierbenjamin73@gmail.com>
2017-05-30 14:27:27 +0200

    ajout comparaison LZMA

--- Projet/Version2/test.sh
+++ Projet/Version2/test.sh
@@ -4,6 +4,9 @@ x=100
 bin/BAMV_LZW -c input/dicoAB.txt output/dicoAB_comp.txt
 a=$(wc -c<input/dicoAB.txt)
 b=$(wc -c<output/dicoAB_comp.txt)
+cp input/dicoAB.txt output/dicoAB.txt
+xz output/dicoAB.txt
+c=$(wc -c<output/dicoAB.txt.xz)
 echo""
 echo "Pour le fichier dicoAB.txt :"
 echo""
@@ -11,6 +14,8 @@ echo "Taille avant compression"
 echo $a
 echo "Taille apres compression"
 echo $b
+echo "Taille apres compression avec LZMA"
+echo $c
 z="($x/$a)"
 echo "Taux de compression"
 echo "$z*$b" |bc -l 
@@ -26,6 +31,9 @@ echo "----------------------------------------------"
 bin/BAMV_LZW -c input/lorem_ipsum.txt output/lorem_ipsum_comp.txt
 a=$(wc -c<input/lorem_ipsum.txt)
 b=$(wc -c<output/lorem_ipsum_comp.txt)
+cp input/lorem_ipsum.txt output/lorem_ipsum.txt
+xz output/lorem_ipsum.txt
+c=$(wc -c<output/lorem_ipsum.txt.xz)
 echo""
 echo "Pour le fichier lorem_ipsum.txt :"
 echo""
@@ -33,6 +41,8 @@ echo "Taille avant compression"
 echo $a
 echo "Taille apres compression"
 echo $b
+echo "Taille apres compression avec LZMA"
+echo $c
 z="($x/$a)"
 echo "Taux de compression"
 echo "$z*$b" |bc -l
